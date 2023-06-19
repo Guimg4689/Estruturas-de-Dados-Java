295f2840ae1869276453ab03f17542d570e13533
Guimg4689 <guilherme.martins20@hotmail.com>
2023-06-19 12:55:32 -0300

    Adicionado Pilha

---
+++ src/one/estruturasdedados/No.java
@@ -0,0 +1,38 @@
+package one.estruturasdedados;
+
+public class No {
+    private int dado;
+    private No prox = null;
+
+    public No(int dado) {
+        this.dado = dado;
+    }
+
+    public No() {
+    }
+
+    public int getDado() {
+        return dado;
+    }
+
+    public void setDado(int dado) {
+        this.dado = dado;
+    }
+
+    public No getProx() {
+        return prox;
+    }
+
+
+
+    public void setProx(No prox) {
+        this.prox = prox;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "dado=" + dado +
+                '}';
+    }
+}
